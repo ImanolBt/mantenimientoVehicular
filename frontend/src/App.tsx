49db4ed88f93b8713c87f8a1bf03f8383718cc99
ImanolBt <96183497+ImanolBt@users.noreply.github.com>
2022-07-27 13:13:24 -0500

    Add files via upload

---
+++ frontend/src/App.tsx
@@ -0,0 +1,47 @@
+import { Routes, Route, Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Home } from "./components/Home";
+import { VehiculoList } from "./components/Vehiculo/VehiculoList";
+import { VehiculoForm } from './components/Vehiculo/VehiculoForm';
+ import { VehiculoCard } from "./components/Vehiculo/VehiculoCard";
+
+
+const title = "Mechanical Workshop";
+const description = "Control de Mantenimiento Vehicular";
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <nav className="navbar navbar-expand navbar-dark bg-dark">   
+        <Link to={"/"}  className="navbar-brand ms-3 ">
+             <img src="https://cdn-icons-png.flaticon.com/512/3339/3339392.png" height="45" width="45" alt="logo"/>
+          
+        </Link>
+        <div className="navbar-nav mr-auto">
+          <li className="nav-item">
+            <Link to={"/vehiculos"} className="nav-link  navbar-brand">
+              Vehículos
+            </Link>
+          </li>   
+          <li className="nav-item">
+            <Link to={"/vehiculos/create"} className="nav-link  navbar-brand">
+              Agregar
+            </Link>
+          </li>         
+        </div>
+      </nav>
+      <div className="container mt-3">
+        <Routes>
+          <Route path="/" element={<Home title={title} description={description} />} />    
+          <Route path="/vehiculos" element={<VehiculoList/>}/>    
+          <Route path="/vehiculos/create" element={<VehiculoForm/>}/>   
+          <Route path="/vehiculos/retrieve/:id" element={<VehiculoCard/>}/>  
+          <Route path="/vehiculos/update/:id" element={<VehiculoForm/>}/>  
+          
+     
+        </Routes>
+      </div>
+    </div>
+  );
+}
+export default App;
